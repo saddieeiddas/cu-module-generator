1c33b711a86634ad146a94edd759fd6b8eec7e41
saddieeiddas <saddie.eiddas@gmail.com>
2015-10-10 04:39:42 +0100

    initial commit

---
+++ app/templates/src/ts/main.tsx
@@ -0,0 +1,11 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/// <reference path="../tsd/tsd.d.ts" />
+
+import * as cu from 'cu-core';
+
+console.log('cu-<%= nameSlug %>');
